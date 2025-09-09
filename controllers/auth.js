eb5dbf71f90febba908102c3ac11fdca0b1802fa
syeskov <sjjj@mail.ru>
2025-09-09 21:41:24 +0300

    Lesson 9

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require("../models/User")
 
 const register = async (req, res) => {
-    res.send("register user")
+    res.send("register user1")
 }
 
 const login = async (req, res) => {
