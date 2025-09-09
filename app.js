bf50f523ff1caac4f859417e77a06772aa631d8b
syeskov <sjjj@mail.ru>
2025-09-09 21:28:00 +0300

    Starting lesson

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const app = express();
+require("dotenv").config()
+require("express-async-errors")
+const express = require("express")
+const app = express()
 
-// error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const authRouter = require("./routes/auth")
+const jobsRouter = require("./routes/jobs")
 
-app.use(express.json());
-// extra packages
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+app.use(express.json())
+
+const connectDB = require("./db/connect")
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-start();
+start()
---
+++ controllers/jobs.js
@@ -0,0 +1,27 @@
+const getAllJobs = async () => {
+    res.send("register user")
+}
+
+const getJob = async () => {
+    res.send("register user")
+}
+
+const createJob = async () => {
+    res.send("register user")
+}
+
+const updateJob = async () => {
+    res.send("register user")
+}
+
+const deleteJob = async () => {
+    res.send("register user")
+}
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+}
