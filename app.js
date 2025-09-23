767e56a7e6c6530984d0dfb5798ca7ab76942829
syeskov <sjjj@mail.ru>
2025-09-23 14:14:14 +0300

    Swagger

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require("cors")
 const xss = require("xss-clean")
 const rateLimiter = require("express-rate-limit")
 
+// Swagger
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
+
 const express = require("express")
 const app = express()
 
@@ -26,6 +31,8 @@ app.use(xss())
 const connectDB = require("./db/connect")
 
 app.use(express.static("public"));
+
+app.use("/api-use", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter)
